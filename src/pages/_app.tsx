c62682a1ce712f2ab0d4d7ef4c24bf97e6ed4a07
James FL <jlacandula@gmail.com>
2022-07-26 20:28:48 +0800

    doc: note to sample theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,35 @@
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
+import { useEffect, useState } from 'react';
 import { DefaultTheme, ThemeProvider } from 'styled-components';
 
 import GlobalStyle from '@/styles/globalstyles';
 
 const theme: DefaultTheme = {
+  // NOTE: Set project style variables in tailwind.config and use them in globalStyles below is just for demo
   colors: {
     primary: '#111',
     secondary: '#0070f3',
   },
 };
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </Head>
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [showChild, setShowChild] = useState(false);
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+
+  if (typeof window === 'undefined') {
+    return <>Not in browser...</>;
+  } else {
+    return (
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
-  );
+    );
+  }
 }
